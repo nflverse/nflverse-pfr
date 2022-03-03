ddf52395dc0b37e064019199d88a2c8890fca156
Tan Ho <tan@tanho.ca>
2022-03-03 11:01:41 -0500

    update combine to use piggyback

--- auto/update_combine.R
+++ auto/update_combine.R
@@ -1,5 +1,12 @@
 pkgload::load_all()
 all_combines <- purrr::map_dfr(2000:nflreadr:::most_recent_season(roster = TRUE),
                                purrr::possibly(pfr_combine,otherwise = tibble::tibble()))
+
+attr(all_combines, "nflverse_timestamp") <- Sys.time()
+attr(all_combines, "nflverse_type") <- "combine measurements"
+
 saveRDS(all_combines, "data/combine.rds")
+arrow::write_parquet(all_combines, "data/combine.parquet")
 readr::write_csv(all_combines, "data/combine.csv")
+
+list.files("data",pattern = "combine", full.names = TRUE) |> nflversedata::nflverse_upload(tag = "combine")
