306eb9026e6fc091a27dd01ad1bc826a524f52f2
Tan Ho <38083823+tanho63@users.noreply.github.com>
2022-03-03 10:37:13 -0500

    Update update_combine.R

--- auto/update_combine.R
+++ auto/update_combine.R
@@ -1,5 +1,5 @@
 pkgload::load_all()
 all_combines <- purrr::map_dfr(2000:nflreadr:::most_recent_season(roster = TRUE),
-                               purrr::possibly(scrape_combine,otherwise = tibble::tibble()))
+                               purrr::possibly(pfr_combine,otherwise = tibble::tibble()))
 saveRDS(all_combines, "data/combine.rds")
 readr::write_csv(all_combines, "data/combine.csv")
