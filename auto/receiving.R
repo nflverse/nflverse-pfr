d54bcae1a8e492b059bac579e1ffa81e8e8da928
Sebastian Carl <mrcaseb@gmail.com>
2023-08-21 17:53:22 +0200

    split by season and combine seasons for nflreadr

--- auto/receiving.R
+++ auto/receiving.R
@@ -1,13 +1,38 @@
-df_advstats <- purrr::map(
+if(Sys.getenv("NFLVERSE_REBUILD", "false") == "true"){
+  seasons_to_update <- 2018:nflreadr::most_recent_season()
+} else {
+  seasons_to_update <- nflreadr::most_recent_season()
+}
+
+purrr::walk(
+  seasons_to_update,
+  purrr::possibly(function(season){
+    nflversedata::nflverse_save(
+      data_frame = pfr_advanced_receiving_season(season),
+      file_name = glue::glue("advstats_season_rec_{season}"),
+      nflverse_type = "advanced receiving season stats via PFR",
+      release_tag = "pfr_advstats",
+      file_types = "rds"
+    )
+  }, quiet = FALSE
+  )
+)
+
+## NOW COMBINE ALL SEASONS FOR THE FILE nflreadr IS LOADING
+
+combined_advstats <- purrr::map(
   2018:nflreadr::most_recent_season(),
-  purrr::possibly(pfr_advanced_receiving_season, tibble::tibble(), quiet = FALSE)
+  purrr::possibly(function(season){
+    load_from <- glue::glue("https://github.com/nflverse/nflverse-data/releases/download/pfr_advstats/advstats_season_rec_{season}.rds")
+    nflreadr::rds_from_url(load_from)
+  }, tibble::tibble(), quiet = FALSE),
+  .progress = TRUE
 ) |>
   purrr::list_rbind()
 
 nflversedata::nflverse_save(
-  data_frame = df_advstats,
+  data_frame = combined_advstats,
   file_name = "advstats_season_rec",
   nflverse_type = "advanced receiving season stats via PFR",
   release_tag = "pfr_advstats"
 )
-
